f86a33998f3fc5f5aa1b0a75b00a63a1b8d77f9b
Neil Shweky <neilshweky@gmail.com>
2022-08-31 14:45:39 -0400

    MONGOID-5226 Allow setting "store_in collection" on document subclass (#5449)
    * MONGOID-5226 Allow setting "store_in collection" on document subclass
    
    * remove comment
    
    * MONGOID-5226 add docs and testing
    
    * Apply suggestions from code review
    
    Co-authored-by: Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>
    
    * MONGOID-5226 answer comments
    
    Co-authored-by: Oleg Pudeyev <39304720+p-mongo@users.noreply.github.com>

--- lib/mongoid/clients/storage_options.rb
+++ lib/mongoid/clients/storage_options.rb
@@ -7,10 +7,7 @@ module Mongoid
       extend ActiveSupport::Concern
 
       included do
-
-        cattr_accessor :storage_options, instance_writer: false do
-          storage_options_defaults
-        end
+        class_attribute :storage_options, instance_writer: false, default: storage_options_defaults
       end
 
       module ClassMethods
@@ -52,7 +49,7 @@ module Mongoid
         # @since 3.0.0
         def store_in(options)
           Validators::Storage.validate(self, options)
-          storage_options.merge!(options)
+          self.storage_options = self.storage_options.merge(options)
         end
 
         # Reset the store_in options
--- lib/mongoid/clients/validators/storage.rb
+++ lib/mongoid/clients/validators/storage.rb
@@ -25,23 +25,9 @@ module Mongoid
         # @since 3.0.0
         def validate(klass, options)
           valid_keys?(options) or raise Errors::InvalidStorageOptions.new(klass, options)
-          valid_parent?(klass) or raise Errors::InvalidStorageParent.new(klass)
         end
 
         private
-        # Determine if the current klass is valid to change store_in
-        # options
-        #
-        # @api private
-        #
-        # @param [ Class ] klass
-        #
-        # @return [ true, false ] If the class is valid
-        #
-        # @since 4.0.0
-        def valid_parent?(klass)
-          !klass.superclass.include?(Mongoid::Document)
-        end
 
         # Determine if all keys in the options hash are valid.
         #
